75e3473456b5968db6c56f03b43d8506ea448e96
martindw26 <martindw97@gmail.com>
2022-11-14 23:08:36 +0000

    img rounded-1

--- includes/section-hp_featured_slider.php
+++ includes/section-hp_featured_slider.php
@@ -22,7 +22,7 @@
   <div class="col-md-6"> <!-- main fetured block-->
  <div class="card rounded rounded-1 border border-0">
 <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
-<img class="card-img-top rounded rounded-0 featured" src="<?php echo $url ?>" alt="Card image cap" style=" object-fit: cover; height:300px">  <div class="card-body">
+<img class="card-img-top rounded rounded-1 featured" src="<?php echo $url ?>" alt="Card image cap" style=" object-fit: cover; height:300px">  <div class="card-body">
     <h4 class="card-title fw-bold"><?php echo get_the_title();?></h4>
 				<h2 class="mb-4" style="height:25px";>	
 				<!-- catarrayrated -->
