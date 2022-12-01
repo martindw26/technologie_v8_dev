d645581a802ad0a6b85fbfa0ab395eb188987d99
martin williams <martindw97@gmail.com>
2022-12-01 18:45:35 +0000

    disclaimer code update v2.1

--- includes/section-disclaimer.php
+++ includes/section-disclaimer.php
@@ -2,7 +2,7 @@
 $disclaimer_text = get_field( 'article_post_content_disclaimer_block_text','option' );
 $disclaimer_url = get_field( 'article_post_content_disclaimer_block_url','option' );
 
-if(get_field('article_post_content_disclaimer_block_text'))
+if(get_field('article_post_content_disclaimer_block_text','option'))
 {
     echo '<p>' . $disclaimer_text . '<a href='.$disclaimer_url.' target="_blank"/>&#9432;</a></p>';
 }
