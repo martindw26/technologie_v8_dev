d4a093104002e5862e9f56ad785e4e4f8710619c
martin williams <martindw97@gmail.com>
2022-12-01 18:34:29 +0000

    updated href php

--- includes/section-disclaimer.php
+++ includes/section-disclaimer.php
@@ -3,5 +3,5 @@
 $disclaimer_text = get_feild( 'article_post_content_disclaimer_block_text','option' );
 $disclaimer_url = get_feild( 'article_post_content_disclaimer_block_url','option' );
 if get_feild( 'article_post_content_disclaimer_block_text','option' ):?>
-    <p><?php echo $disclaimer_text;?><a href="'.$disclaimer_url.'">&#9432;</a></p>
+    <p><?php echo $disclaimer_text;?><a href='<?php echo $disclaimer_text;?>'>&#9432;</a></p>
 <?php endif;?>    
