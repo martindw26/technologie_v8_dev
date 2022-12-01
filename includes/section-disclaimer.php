d5dd646f5df8f6d412348c83246a46579225b895
martin williams <martindw97@gmail.com>
2022-12-01 18:32:21 +0000

    field

--- includes/section-disclaimer.php
+++ includes/section-disclaimer.php
@@ -1,7 +1,7 @@
 
 <?php 
-$disclaimer_text = get_field( 'article_post_content_disclaimer_block_text','option' );
-$disclaimer_url = get_field( 'article_post_content_disclaimer_block_url','option' );
+$disclaimer_text = get_feild( 'article_post_content_disclaimer_block_text','option' );
+$disclaimer_url = get_feild( 'article_post_content_disclaimer_block_url','option' );
 if get_feild( 'article_post_content_disclaimer_block_text','option' ):?>
     <p><?php echo $disclaimer_text;?><a href="'.$disclaimer_url.'">&#9432;</a></p>
 <?php endif;?>    
