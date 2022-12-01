4d2818558b8bc075346c84b759a3db59320320e8
martin williams <martindw97@gmail.com>
2022-12-01 18:44:23 +0000

    disclaimer code update v2

--- includes/section-disclaimer.php
+++ includes/section-disclaimer.php
@@ -1,7 +1,10 @@
+<?php
+$disclaimer_text = get_field( 'article_post_content_disclaimer_block_text','option' );
+$disclaimer_url = get_field( 'article_post_content_disclaimer_block_url','option' );
 
-<?php 
-$disclaimer_text = get_feild( 'article_post_content_disclaimer_block_text','option' );
-$disclaimer_url = get_feild( 'article_post_content_disclaimer_block_url','option' );
-if get_feild( 'article_post_content_disclaimer_block_text','option' ):?>
-    <p><?php echo $disclaimer_text;?><a href='<?php echo $disclaimer_text;?>'>&#9432;</a></p>
-<?php endif;?>    
+if(get_field('article_post_content_disclaimer_block_text'))
+{
+    echo '<p>' . $disclaimer_text . '<a href='.$disclaimer_url.' target="_blank"/>&#9432;</a></p>';
+}
+
+?>
