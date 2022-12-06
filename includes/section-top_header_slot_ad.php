4f9f6289de48ade7786c19998bfaa36342e77413
martin williams <martindw97@gmail.com>
2022-12-06 23:04:29 +0000

    removed old ad file and updated ad slot background colour

--- includes/section-top_header_slot_ad.php
+++ includes/section-top_header_slot_ad.php
@@ -1,5 +1,4 @@
 <div class="commerce_hp_desktop">
-<div class="bg-light">
 <div class="commerce_ldr bg-secondary rounded rounded-1 mb-4" style="height:280px; width:980px; margin: 0 auto;">
 <div><center>
 <p class="d-flex justify-content-center pt-2 text-light">Advertisment</p>
@@ -13,10 +12,8 @@ echo $Header_Top_Slot;
 </center></div> 
 </div>
 </div>
-</div>   
 
 <div class="commerce_hp_mobile">
-<div class="bg-light">
 <div class="commerce_mobile bg-secondary rounded rounded-1 mb-4" style="height:120px; width:370px; margin: 0 auto;">
 <div><center>
 <p class="d-flex justify-content-center pt-2 text-light">Advertisment</p>
@@ -28,6 +25,5 @@ echo $Header_Top_Slot;
 ?>
 
 </center></div> 
-</div>
 </div>
 </div>
