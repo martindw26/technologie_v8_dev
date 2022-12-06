bd7f2b4ce2e187338b25cbd75386053a48c679e7
martin williams <86750566+martindw26@users.noreply.github.com>
2022-12-06 19:47:50 +0000

    Add files via upload
    added files

---
+++ includes/section-top_header_slot_ad.php
@@ -0,0 +1,28 @@
+<div class="bg-light">
+<div class="commerce_ldr bg-secondary rounded rounded-1 mb-4" style="height:280px; width:980px; margin: 0 auto;">
+<div><center>
+<p class="d-flex justify-content-center pt-2 text-light">Advertisment</p>
+<?php 
+
+$Header_Top_Slot  = get_field( 'header_ad_slot','option' );
+echo $Header_Top_Slot;
+
+?>
+
+</center></div> 
+</div></div>   
+
+
+<div class="bg-light">
+<div class="commerce_mobile bg-secondary rounded rounded-1 mb-4" style="height:120px; width:370px; margin: 0 auto;">
+<div><center>
+<p class="d-flex justify-content-center pt-2 text-light">Advertisment</p>
+<?php 
+
+$Header_Top_Slot  = get_field( 'header_ad_slot','option' );
+echo $Header_Top_Slot;
+
+?>
+
+</center></div> 
+</div></div>   
