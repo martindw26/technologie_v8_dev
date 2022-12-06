2e1fad5881829e91179476349f6925928654c893
martin williams <martindw97@gmail.com>
2022-12-06 19:26:17 +0000

    Revert "manual update 06/12"
    This reverts commit f8688041ba67cf760a29c8b5cafe26c985883603.

--- template-parts/adshead.php
+++
@@ -1,9 +0,0 @@
-<!-- MPU Bottom ad head script -->
-<?php 
-$MPU_bottom_ad_script = get_field ( 'mpu_bottom_slot_head_script','option' );
-echo $MPU_bottom_ad_script;
-?>
-<!-- END MPU Bottom ad head script -->
-
-
-              
