872ab14fbbaecfbcd7558c7478e0f47ed6d31511
martin williams <martindw97@gmail.com>
2022-12-06 19:27:20 +0000

    front page update

--- front-page.php
+++ front-page.php
@@ -6,17 +6,28 @@
 
 
                         <!-- Featured Post-->
-	       <?php get_template_part('includes/section','hp_featured_slider');?><hr class="container">
-
+	       		<?php get_template_part('includes/section','hp_featured_slider');?>
                         <!-- End Featured Post-->
+
+				<!-- Header adslot-->
+	       		<?php get_template_part('includes/section','top_header_slot_ad');?>
+                 <!-- End Featured Post-->
                         
                         <!-- Reviews Post Block-->
                         <?php get_template_part('includes/section','non_featured_reviews');?><hr class="container">
                         <!-- End of Reviews Post Block--> 
+
+				<!-- Header middle adslot-->
+	       		<?php get_template_part('includes/section','middle_header_slot_ad');?>
+                 <!-- End Header middle adslot-->
                                     
                         <!-- Projects Post Block-->
                         <?php get_template_part('includes/section','non_featured_projects');?><hr class="container">
                         <!-- End of Projects Post Block-->
+
+				<!-- Header bottom adslot-->
+	       		<?php get_template_part('includes/section','bottom_header_slot_ad');?>
+                 <!-- End Header bottom adslot-->
 
                         <!-- Projects Post Block-->
                         <?php get_template_part('includes/section','homepage_deals_block');?>
