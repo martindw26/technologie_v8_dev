65e8f5def4fe3a4dd199baaa4502187563fb02e0
martin williams <martindw97@gmail.com>
2022-12-06 23:22:40 +0000

    created Header_Bottom_Slot

---
+++ includes/section-bottom_header_slot_ad.php
@@ -0,0 +1,29 @@
+<div class="commerce_hp_desktop">
+<div class="commerce_ldr rounded rounded-1 mb-4" style="height:280px; width:980px; margin: 0 auto; background-color: #dee2e6;">
+<div><center>
+<p class="d-flex justify-content-center pt-2 text-dark">Advertisment</p>
+<?php 
+
+$Header_Bottom_Slot  = get_field( 'header_ad_bottom_slot_body_script','option' );
+echo $Bottom_Top_Slot;
+
+?>
+
+</center></div> 
+</div>
+</div>
+
+<div class="commerce_hp_mobile">
+<div class="commerce_mobile rounded rounded-1 mb-4" style="height:120px; width:370px; margin: 0 auto; background-color: #dee2e6;">
+<div><center>
+<p class="d-flex justify-content-center pt-2 text-dark">Advertisment</p>
+<?php 
+
+$Header_Bottom_Slot  = get_field( 'header_ad_bottom_slot_body_script','option' );
+echo $Bottom_Top_Slot;
+
+?>
+
+</center></div> 
+</div>
+</div>
