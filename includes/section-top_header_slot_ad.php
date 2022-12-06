bbce55d99a14f7f6ec86894548d2b76ebfccbe7c
martin williams <martindw97@gmail.com>
2022-12-06 23:12:52 +0000

    text dark

--- includes/section-top_header_slot_ad.php
+++ includes/section-top_header_slot_ad.php
@@ -1,7 +1,7 @@
 <div class="commerce_hp_desktop">
 <div class="commerce_ldr rounded rounded-1 mb-4" style="height:280px; width:980px; margin: 0 auto; background-color: #dee2e6;">
 <div><center>
-<p class="d-flex justify-content-center pt-2 text-light">Advertisment</p>
+<p class="d-flex justify-content-center pt-2 text-dark">Advertisment</p>
 <?php 
 
 $Header_Top_Slot  = get_field( 'header_ad_slot','option' );
@@ -16,7 +16,7 @@ echo $Header_Top_Slot;
 <div class="commerce_hp_mobile">
 <div class="commerce_mobile rounded rounded-1 mb-4" style="height:120px; width:370px; margin: 0 auto; background-color: #dee2e6;">
 <div><center>
-<p class="d-flex justify-content-center pt-2 text-light">Advertisment</p>
+<p class="d-flex justify-content-center pt-2 text-dark">Advertisment</p>
 <?php 
 
 $Header_Top_Slot  = get_field( 'header_ad_slot','option' );
