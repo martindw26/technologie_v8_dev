c9c16a56427b0c606296d3e26993091f201f1a9d
martin williams <86750566+martindw26@users.noreply.github.com>
2022-12-06 19:43:01 +0000

    Add files via upload
    updated adshead and reviews_sidebar

---
+++ template-parts/adshead.php
@@ -0,0 +1,9 @@
+<!-- MPU Bottom ad head script -->
+<?php 
+$MPU_bottom_ad_script = get_field ( 'mpu_bottom_slot_head_script','option' );
+echo $MPU_bottom_ad_script;
+?>
+<!-- END MPU Bottom ad head script -->
+
+
+              
