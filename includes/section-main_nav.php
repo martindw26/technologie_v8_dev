5ddb7768e7daaed12258ae05e702400b9b5feed0
martin williams <martindw97@gmail.com>
2022-12-07 13:59:24 +0000

    bs nave bar update 2

--- includes/section-main_nav.php
+++ includes/section-main_nav.php
@@ -10,7 +10,6 @@
 
         <nav class="navbar navbar-expand-lg bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar w/ text</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -26,9 +25,6 @@
               <a class="nav-link" href="#">Pricing</a>
             </li>
           </ul>
-          <span class="navbar-text">
-            Navbar text with an inline element
-          </span>
         </div>
       </div>
     </nav>
