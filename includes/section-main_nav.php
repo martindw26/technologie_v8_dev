c0803c48e2fc835f7e44976d30709769c414f10e
martin williams <martindw97@gmail.com>
2022-12-07 15:10:28 +0000

    new responsive nav v1.2

--- includes/section-main_nav.php
+++ includes/section-main_nav.php
@@ -1,27 +1,12 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-<div class="container">
-<div class="topnav" id="myTopnav">
-	<?php
+<nav id="nav-bar">
+  <div class="menu">
+  <?php
 	wp_nav_menu(
 		array(
 		'theme_location' => 'primary',
-		'menu_class' => 'topnav' 
+		'menu_class' => 'nav-bar' 
 		)
 		);
 	?>
-</div>
-</div>
-
-
-<script>
-    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-</script>
+  </div>
+</nav>
